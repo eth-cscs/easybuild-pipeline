b579ad1b370b5a57ca1399da28eb1bdce0636cab
teojgo <manitaras@cscs.ch>
2017-09-07 12:05:39 +0200

    Update ci-runner.sh

--- ci-runner.sh
+++ ci-runner.sh
@@ -6,7 +6,8 @@ project_name=${JOB_NAME%/*}
 
 # hard-coded list of systems in project
 syslist="daint dom kesch leone monch"
-
+export COMMIT=`git rev-parse --short HEAD`
+echo ${COMMIT}
 # exit status
 status=0
 
